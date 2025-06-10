0a7aee00ef9cc6785f4bc329ac822b070a78ab90
John Kline <jpkline43@gmail.com>
2025-06-10 17:37:25 -0400

    Multi-add works!

--- test.py
+++ test.py
@@ -185,6 +185,36 @@ def embed_file_structure(excel_file, relpaths, basepath=None):
     print(f"✅ Success: File embedded in {excel_file}")
 
 
+def embed_file_structure(base_xlsx, files_to_embed):
+    with mutablezip.MutableZipFile(base_xlsx, "a", compression=zipfile.ZIP_DEFLATED) as zf:
+        tree = ET.parse(io.BytesIO(zf.read("[Content_Types].xml")))
+        root = tree.getroot()
+        ns = {"ct": "http://schemas.openxmlformats.org/package/2006/content-types"}
+        ET.register_namespace("", ns["ct"])
+
+        for embedded_path in files_to_embed:
+            embedded_path = pathlib.Path(embedded_path).as_posix().lstrip("/")
+            if embedded_path in zf.namelist():
+                zf.read(embedded_path)  # Allows MutableZip to overwrite it later somehow
+            with open(embedded_path, "rb") as f:
+                zf.writestr(embedded_path, f.read())
+
+            # Avoid duplicate entries
+            existing = any(el.attrib.get("PartName") == "/" + embedded_path for el in root.findall("ct:Override", ns))
+
+            if not existing:
+                ET.SubElement(
+                    root,
+                    r"{http://schemas.openxmlformats.org/package/2006/content-types}Override",
+                    {"PartName": "/" + embedded_path, "ContentType": "application/xml"},
+                )
+        outf = io.BytesIO()
+        tree.write(outf, xml_declaration=True, encoding="utf-8")
+        zf.writestr("[Content_Types].xml", outf.getvalue())
+
+    print(f"✅ Success: File embedded in {base_xlsx}")
+
+
 # === USAGE ===
 
 embed_file_in_xlsx(
@@ -193,3 +223,7 @@ embed_file_in_xlsx(
     embedded_path="/scraped_programs/Art_(BA).xml",  # Where it will be stored inside the .xlsx
     output_xlsx="patched.xlsx",  # Output file
 )
+
+import glob
+
+embed_file_structure("clean multi.xlsx", glob.glob("scraped_programs/*.xml"))
