10eeadc940c1e9c82216367ab532d0324764e304
John Kline <jpkline43@gmail.com>
2025-06-19 21:58:43 -0400

    Some progress

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,22 @@ mod schedule;
 mod write_excel_file;
 use read_excel_file::read_file;
 use read_self_zip::load_programs;
-use schedule::generate_schedule_df;
+use schedule::generate_schedule;
 use write_excel_file::save_schedule;
 
 fn main() -> Result<()> {
     const FNAME: &str = "schedulebot_test.xlsx";
 
     let dataframes = load_programs()?;
-    let full_df = generate_schedule_df(&dataframes);
-    save_schedule(&Path::new(FNAME).to_path_buf(), &full_df, &dataframes)?;
+    let sched = generate_schedule(dataframes.keys().map(|x| x.as_str()).collect(), &dataframes)?;
+    save_schedule(&Path::new(FNAME).to_path_buf(), &sched.df, &sched.catalog)?;
 
     println!("Excel file created: {}", FNAME);
 
     let new_df = read_file(&Path::new(FNAME).to_path_buf()).unwrap();
     println!("Read file: {}", FNAME);
 
-    save_schedule(&Path::new("output.xlsx").to_path_buf(), &full_df, &new_df)?;
+    save_schedule(&Path::new("output.xlsx").to_path_buf(), &sched.df, &new_df)?;
 
     println!("Excel file created: output.xlsx");
     Ok(())
--- src/schedule.rs
+++ src/schedule.rs
@@ -3,11 +3,25 @@ use indexmap::IndexMap;
 use polars::prelude::*;
 use std::{collections::HashMap, path::Path};
 
-pub fn generate_schedule_df(dfs: &HashMap<String, DataFrame>) -> DataFrame {
-    let full_df_list: Vec<DataFrame> = dfs.values().cloned().collect();
+pub struct Schedule {
+    pub df: DataFrame,
+    pub programs: Vec<String>,
+    pub catalog: HashMap<String, DataFrame>,
+}
+
+pub fn generate_schedule(
+    programs: Vec<&str>,
+    catalog: &HashMap<String, DataFrame>,
+) -> Result<Schedule> {
+    // (catalog: )
+    let full_df_list: Vec<DataFrame> = catalog
+        .into_iter()
+        .filter_map(|(k, v)| programs.contains(&k.as_str()).then_some(v))
+        .cloned()
+        .collect();
 
     let mut all_columns: IndexMap<String, DataType> = IndexMap::new();
-    for df in &full_df_list {
+    for df in full_df_list.iter() {
         all_columns.extend(
             df.get_column_names()
                 .iter()
@@ -16,28 +30,29 @@ pub fn generate_schedule_df(dfs: &HashMap<String, DataFrame>) -> DataFrame {
         );
     }
 
-    let dfs_aligned: Vec<DataFrame> = full_df_list
+    let dfs_aligned = full_df_list
         .into_iter()
         .map(|mut df| {
             for (col, dtype) in &all_columns {
                 if !df.get_column_names().contains(&col.as_str()) {
                     let s = Series::full_null(col, df.height(), dtype);
-                    df.with_column(s).unwrap();
+                    df.with_column(s)?;
                 }
             }
-            df.select(all_columns.iter().map(|x| x.0).collect::<Vec<_>>())
-                .unwrap()
+            Ok(df.select(all_columns.iter().map(|x| x.0).collect::<Vec<_>>())?)
         })
-        .collect();
+        .collect::<Result<Vec<_>>>()?;
 
-    concat(
-        dfs_aligned
-            .into_iter()
-            .map(|x| x.lazy())
-            .collect::<Vec<_>>(),
-        UnionArgs::default(),
-    )
-    .unwrap()
-    .collect()
-    .unwrap()
+    Ok(Schedule {
+        df: concat(
+            dfs_aligned
+                .into_iter()
+                .map(|x| x.lazy())
+                .collect::<Vec<_>>(),
+            UnionArgs::default(),
+        )?
+        .collect()?,
+        programs: programs.into_iter().map(|v| v.to_string()).collect(),
+        catalog: catalog.clone(),
+    })
 }
