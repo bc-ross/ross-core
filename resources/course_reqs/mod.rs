22e41500b65a8df7e7bf2b8ca4ee15a1e2a80671
TimKasprzak <kasp1772@ravens.benedictine.edu>
2025-08-05 23:34:32 -0500

    added prereqs and coreqs for architecture

--- resources/course_reqs/mod.rs
+++ resources/course_reqs/mod.rs
@@ -3,14 +3,20 @@ use crate::schedule::CourseCode;
 use std::collections::HashMap;
 use std::iter::empty;
 
+mod stem_acct;
+mod stem_arch;
 mod stem_astr;
 mod stem_biol;
 mod stem_chem;
+mod stem_csci;
 
 pub fn prereqs() -> HashMap<CourseCode, CourseReq> {
     empty()
+        .chain(stem_acct::prereqs().into_iter())
+        .chain(stem_arch::prereqs().into_iter())
         .chain(stem_astr::prereqs().into_iter())
         .chain(stem_biol::prereqs().into_iter())
         .chain(stem_chem::prereqs().into_iter())
+        .chain(stem_csci::prereqs().into_iter())
         .collect()
 }
