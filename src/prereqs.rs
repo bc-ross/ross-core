6012df90f2c1ea67fbebdb5cd81f5e8d016944d1
John Kline <jpkline43@gmail.com>
2025-08-06 00:07:57 -0400

    Fix coreq implementation

--- src/prereqs.rs
+++ src/prereqs.rs
@@ -149,10 +149,9 @@ impl CourseReq {
                 .any(|c| c == code),
             CourseReq::CoCourse(code) | CourseReq::CoCourseGrade(code, _) => sched
                 .courses
-                .iter()
-                .take(sem_idx + 1)
-                .flatten()
-                .any(|c| c == code),
+                .get(sem_idx)
+                .filter(|sem| sem.contains(code))
+                .is_some(),
             CourseReq::Program(x) => sched.programs.iter().any(|p| {
                 sched
                     .catalog
