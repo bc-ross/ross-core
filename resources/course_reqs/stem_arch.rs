72f36e760e01cd35f713a0dd397981f9fdc30ecf
John Kline <jpkline43@gmail.com>
2025-08-06 01:16:52 -0400

    Update default stem

--- resources/course_reqs/stem_arch.rs
+++ resources/course_reqs/stem_arch.rs
@@ -10,17 +10,17 @@ use crate::{CC, GR};
 pub fn prereqs() -> Vec<(CourseCode, CourseReq)> {
     vec![
         (
-            CC!("stem ARCH", 1200),
+            CC!("ARCH", 1200),
             Or(vec![
                 PreCourse(CC!("ART", 1000)),
                 CoCourse(CC!("ART", 1000)),
             ]),
         ),
         (
-            CC!("stem ARCH", 1410),
+            CC!("ARCH", 1410),
             And(vec![
                 Or(vec![
-                    PreCourse(CC!("STEM ARCH", 1300)),
+                    PreCourse(CC!("ARCH", 1300)),
                     PreCourse(CC!("ART", 1000)),
                 ]),
                 Or(vec![
@@ -36,60 +36,60 @@ pub fn prereqs() -> Vec<(CourseCode, CourseReq)> {
         (
             CC!("stem ARCH", 2111),
             And(vec![
-                PreCourse(CC!("STEM ARCH", 1200)),
-                PreCourse(CC!("STEM ARCH", 1410)),
+                PreCourse(CC!("ARCH", 1200)),
+                PreCourse(CC!("ARCH", 1410)),
                 PreCourse(CC!("MATH", 1300)),
-                CoCourse(CC!("STEM ARCH", 2201)),
+                CoCourse(CC!("ARCH", 2201)),
             ]),
         ),
         (
             CC!("stem ARCH", 2112),
-            PreCourseGrade(CC!("STEM ARCH", 2111), GR!(C)),
+            PreCourseGrade(CC!("ARCH", 2111), GR!(C)),
         ),
         (
             CC!("stem ARCH", 2201),
             And(vec![
                 PreCourse(CC!("ART", 1000)),
-                PreCourse(CC!("STEM ARCH", 1200)),
-                PreCourse(CC!("STEM ARCH", 1410)),
+                PreCourse(CC!("ARCH", 1200)),
+                PreCourse(CC!("ARCH", 1410)),
             ]),
         ),
-        (CC!("stem ARCH", 3100), PreCourse(CC!("STEM ARCH", 2112))),
+        (CC!("ARCH", 3100), PreCourse(CC!("ARCH", 2112))),
         (
-            CC!("stem ARCH", 3113),
+            CC!("ARCH", 3113),
             And(vec![
-                PreCourse(CC!("STEM ARCH", 2112)),
-                PreCourse(CC!("STEM ARCH", 2301)),
+                PreCourse(CC!("ARCH", 2112)),
+                PreCourse(CC!("ARCH", 2301)),
                 Or(vec![
                     PreCourse(CC!("PHYS", 2000)),
                     PreCourse(CC!("PHYS", 2100)),
                 ]),
                 Or(vec![
-                    PreCourse(CC!("STEM ARCH", 2300)),
-                    CoCourse(CC!("STEM ARCH", 2300)),
+                    PreCourse(CC!("ARCH", 2300)),
+                    CoCourse(CC!("ARCH", 2300)),
                 ]),
             ]),
         ),
         (
-            CC!("stem ARCH", 3114),
-            PreCourseGrade(CC!("STEM ARCH", 3113), GR!(C)),
+            CC!("ARCH", 3114),
+            PreCourseGrade(CC!("ARCH", 3113), GR!(C)),
         ),
-        (CC!("stem ARCH", 3200), PreCourse(CC!("STEM ARCH", 2201))),
-        (CC!("stem ARCH", 3310), PreCourse(CC!("STEM ARCH", 2112))),
+        (CC!("ARCH", 3200), PreCourse(CC!("ARCH", 2201))),
+        (CC!("ARCH", 3310), PreCourse(CC!("ARCH", 2112))),
         (
-            CC!("stem ARCH", 3400),
+            CC!("ARCH", 3400),
             And(vec![
-                PreCourse(CC!("STEM ARCH", 2301)),
-                PreCourse(CC!("STEM ARCH", 2112)),
+                PreCourse(CC!("ARCH", 2301)),
+                PreCourse(CC!("ARCH", 2112)),
             ]),
         ),
         (
-            CC!("stem ARCH", 4116),
+            CC!("ARCH", 4116),
             And(vec![
-                PreCourse(CC!("STEM ARCH", 4115)),
-                PreCourse(CC!("STEM ARCH", 4400)),
+                PreCourse(CC!("ARCH", 4115)),
+                PreCourse(CC!("ARCH", 4400)),
             ]),
         ),
-        (CC!("stem ARCH", 4400), PreCourse(CC!("STEM ARCH", 3113))),
+        (CC!("ARCH", 4400), PreCourse(CC!("ARCH", 3113))),
     ]
 }
