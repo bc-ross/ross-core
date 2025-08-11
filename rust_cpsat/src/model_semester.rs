a7d8de24e92e7634daa490fc144bef97da24fbef
John Kline <jpkline43@gmail.com>
2025-08-11 19:11:33 -0400

    Add semester constraints

--- rust_cpsat/src/model_semester.rs
+++ rust_cpsat/src/model_semester.rs
@@ -1,6 +1,15 @@
-//! Functions for adding generic semester constraints (e.g., max credits).
+/// Functions for adding generic semester constraints (e.g., max credits).
 use crate::model_context::ModelBuilderContext;
 
 pub fn add_semester_constraints<'a>(ctx: &mut ModelBuilderContext<'a>) {
-    // TODO: Move semester credit logic here from model.rs/main.rs
+    // For each semester, sum the credits of all courses scheduled and add a constraint
+    for s in 0..ctx.num_semesters {
+        let weighted_terms: Vec<(i64, _)> = ctx.courses
+            .iter()
+            .enumerate()
+            .map(|(i, c)| (c.credits, ctx.vars[i][s]))
+            .collect();
+        let weighted_sum: cp_sat::builder::LinearExpr = weighted_terms.into_iter().collect();
+        ctx.model.add_le(weighted_sum, ctx.max_credits_per_semester);
+    }
 }
