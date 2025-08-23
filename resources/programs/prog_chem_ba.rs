ed68e7aba26421b1bb17f5d11f36f9c3f9777d75
John Kline <jpkline43@gmail.com>
2025-08-23 16:33:50 -0500

    Add elective for Chem

--- resources/programs/prog_chem_ba.rs
+++ resources/programs/prog_chem_ba.rs
@@ -41,6 +41,23 @@ pub fn prog() -> Program {
             vec![CC!("CHEM", 4811), CC!("CHEM", 4903), CC!("CHEM", "COMP")],
         ],
         assoc_stems: vec!["CHEM".to_string()],
-        electives: vec![],
+        electives: vec![Elective {
+            name: "Advanced Courses".to_string(),
+            req: Courses {
+                num: 2,
+                courses: vec![
+                    CC!("CHEM", 3150),
+                    CC!("CHEM", 3250),
+                    CC!("CHEM", 3510),
+                    CC!("CHEM", 3650),
+                    CC!("CHEM", 3980),
+                    CC!("CHEM", 4980),
+                    CC!("CHEM", 4350),
+                    CC!("CHEM", 4450),
+                    CC!("CHEM", 4650),
+                    CC!("CHEM", 4200),
+                ],
+            },
+        }],
     }
 }
