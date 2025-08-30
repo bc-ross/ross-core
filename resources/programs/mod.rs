502c2ae3b192249656376a57e236234f3378862d
Julia <ferd9085.ravens@gmail.com>
2025-08-29 23:17:41 -0500

    finished basic reqs for Art BA

--- resources/programs/mod.rs
+++ resources/programs/mod.rs
@@ -1,6 +1,7 @@
 use crate::schedule::Program;
 mod prog_acctba;
 mod prog_arch_ba;
+mod prog_art_ba;
 mod prog_chem_ba;
 mod prog_phys_ba;
 
@@ -8,6 +9,7 @@ pub fn programs() -> Vec<Program> {
     vec![
         prog_acctba::prog(),
         prog_arch_ba::prog(),
+        prog_art_ba::prog(),
         prog_chem_ba::prog(),
         prog_phys_ba::prog(),
     ]
