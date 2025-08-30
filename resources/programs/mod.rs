68dab2693c803810ed29bb6a9dfcc5fec4a09361
Julia <ferd9085.ravens@gmail.com>
2025-08-29 22:11:28 -0500

    finished basic reqs for Accounting

--- resources/programs/mod.rs
+++ resources/programs/mod.rs
@@ -1,7 +1,12 @@
 use crate::schedule::Program;
+mod prog_acctba;
 mod prog_chem_ba;
 mod prog_phys_ba;
 
 pub fn programs() -> Vec<Program> {
-    vec![prog_chem_ba::prog(), prog_phys_ba::prog()]
+    vec![
+        prog_acctba::prog(),
+        prog_chem_ba::prog(),
+        prog_phys_ba::prog(),
+    ]
 }
