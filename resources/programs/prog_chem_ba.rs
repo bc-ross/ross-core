2ef6e368f761b3633fac166ba98d32ef16e559d6
Julia <ferd9085.ravens@gmail.com>
2025-08-30 22:14:01 -0500

    finished basic reqs for chemistry ba

--- resources/programs/prog_chem_ba.rs
+++ resources/programs/prog_chem_ba.rs
@@ -37,27 +37,11 @@ pub fn prog() -> Program {
                 CC!("CHEM", 3801),
                 CC!("CHEM", 4901),
             ],
-            vec![CC!("CHEM", 4801), CC!("CHEM", 4902)],
-            vec![CC!("CHEM", 4811), CC!("CHEM", 4903), CC!("CHEM", "COMP")],
+            vec![CC!("CHEM", 4902)],
+            vec![CC!("CHEM", 4903), CC!("CHEM", "COMP")],
         ],
         assoc_stems: vec!["CHEM".to_string()],
-        electives: vec![Elective {
-            name: "Advanced Courses".to_string(),
-            req: Courses {
-                num: 2,
-                courses: vec![
-                    CC!("CHEM", 3150),
-                    CC!("CHEM", 3250),
-                    CC!("CHEM", 3510),
-                    CC!("CHEM", 3650),
-                    CC!("CHEM", 3980),
-                    CC!("CHEM", 4980),
-                    CC!("CHEM", 4350),
-                    CC!("CHEM", 4450),
-                    CC!("CHEM", 4650),
-                    CC!("CHEM", 4200),
-                ],
-            },
-        }],
+        electives: vec![],
     }
 }
+// Elective info: two from: CHEM-3150, CHEM-3250, CHEM-3510, CHEM-3650, CHEM-3980, CHEM-4980, CHEM-4350, CHEM-4450, CHEM-4650, CHEM-4200
