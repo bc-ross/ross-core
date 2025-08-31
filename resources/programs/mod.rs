6fd894611a667d86f4c4c092b493724dd7d93479
Julia <ferd9085.ravens@gmail.com>
2025-08-30 22:27:07 -0500

    finished basic reqs for chem bs

--- resources/programs/mod.rs
+++ resources/programs/mod.rs
@@ -9,6 +9,7 @@ mod prog_bio_bs;
 mod prog_biochem_ba;
 mod prog_biochem_bs;
 mod prog_chem_ba;
+mod prog_chem_bs;
 mod prog_cheng_bs;
 mod prog_phys_ba;
 
@@ -24,6 +25,7 @@ pub fn programs() -> Vec<Program> {
         prog_bio_ba::prog(),
         prog_bio_bs::prog(),
         prog_chem_ba::prog(),
+        prog_chem_bs::prog(),
         prog_cheng_bs::prog(),
         prog_phys_ba::prog(),
     ]
---
+++ resources/programs/prog_chem_bs.rs
@@ -0,0 +1,52 @@
+#![allow(unused_imports)]
+
+use crate::geneds::ElectiveReq::*;
+use crate::schedule::{CourseCode, Elective, Program};
+use crate::CC;
+
+pub fn prog() -> Program {
+    Program {
+        name: "BS Chemistry".to_string(),
+        semesters: vec![
+            vec![CC!("CHEM", 1200), CC!("CHEM", 1201), CC!("MATH", 1300)],
+            vec![CC!("CHEM", 1210), CC!("CHEM", 1211), CC!("MATH", 1350)],
+            vec![
+                CC!("CHEM", 2200),
+                CC!("CHEM", 2201),
+                CC!("PHYS", 2100),
+                CC!("PHYS", 2101),
+            ],
+            vec![
+                CC!("CHEM", 2210),
+                CC!("CHEM", 2211),
+                CC!("PHYS", 2110),
+                CC!("PHYS", 2111),
+            ],
+            vec![
+                CC!("CHEM", 3300),
+                CC!("CHEM", 3301),
+                CC!("CHEM", 3500),
+                CC!("CHEM", 3501),
+                CC!("CHEM", 4900),
+            ],
+            vec![
+                CC!("CHEM", 3311),
+                CC!("CHEM", 3400),
+                CC!("CHEM", 3401),
+                CC!("CHEM", 3800),
+                CC!("CHEM", 3801),
+                CC!("CHEM", 4901),
+            ],
+            vec![
+                CC!("CHEM", 4801),
+                CC!("CHEM", 4200),
+                CC!("CHEM", 4201),
+                CC!("CHEM", 4902),
+            ],
+            vec![CC!("CHEM", "COMP"), CC!("CHEM", 4811), CC!("CHEM", 4903)],
+        ],
+        assoc_stems: vec!["CHEM".to_string()],
+        electives: vec![],
+    }
+}
+// Elective info: two from: CHEM-3150, CHEM-3250, CHEM-3510, CHEM-3650, CHEM-3980, CHEM-4350, CHEM-4450, CHEM-4650
