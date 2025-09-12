200193dfb3c17a68844a7ec84e4243be05f0d7ea
John Kline <jpkline43@gmail.com>
2025-09-12 17:13:09 -0500

    Remove extraneous checks

--- src/model/courses.rs
+++ src/model/courses.rs
@@ -66,12 +66,12 @@ pub fn add_courses<'a>(ctx: &mut ModelBuilderContext<'a>) {
 
 fn get_term_constraint<'a>(offering: Option<&'a CourseTermOffering>, s: usize) -> bool {
     match offering {
-        Some(crate::schedule::CourseTermOffering::Fall) => (s == 0) || (s % 2 == 1), // odd semesters
-        Some(crate::schedule::CourseTermOffering::Spring) => (s == 0) || (s % 2 == 0), // even semesters
-        Some(crate::schedule::CourseTermOffering::Both) => (s == 0) || true,
-        Some(crate::schedule::CourseTermOffering::Discretion) => (s == 0) || true, // allowed, but may change in future
-        Some(crate::schedule::CourseTermOffering::Infrequently) => (s == 0) || true, // allowed, but may change in future
-        Some(crate::schedule::CourseTermOffering::Summer) => (s == 0) || false, // never schedule
-        None => true,                                                           // default: allow
+        Some(crate::schedule::CourseTermOffering::Fall) => (s % 2 == 1), // odd semesters
+        Some(crate::schedule::CourseTermOffering::Spring) => (s % 2 == 0), // even semesters
+        Some(crate::schedule::CourseTermOffering::Both) => true,
+        Some(crate::schedule::CourseTermOffering::Discretion) => true, // allowed, but may change in future
+        Some(crate::schedule::CourseTermOffering::Infrequently) => true, // allowed, but may change in future
+        Some(crate::schedule::CourseTermOffering::Summer) => false,      // never schedule
+        None => true,                                                    // default: allow
     }
 }
